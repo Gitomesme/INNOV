d67d79c581305921c11f7782de6cab43ab1b9d77
Gitomesme <tom.sarrant@esme.fr>
2022-03-28 15:45:36 +0200

    changement de fenetre

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,8 +18,12 @@ public class MainActivity extends AppCompatActivity {
         Conex.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+
                 Conex.setText("hello world");
             }
+
         });
 
     }
